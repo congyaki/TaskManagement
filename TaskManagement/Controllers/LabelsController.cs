bd30becaa4baedb487c0bf66f8b13fd505bb084d
TranTamdeptrai <94815751+TranTamdeptrai@users.noreply.github.com>
2024-11-11 11:23:11 +0700

    FE-task

--- TaskManagement/Controllers/LabelsController.cs
+++ TaskManagement/Controllers/LabelsController.cs
@@ -88,7 +88,7 @@ namespace TaskManagement.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Code,Name,Color,CreatedBy,CreatedAt,LastModifiedBy,LastModifiedAt")] TblDmLabel label)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Code,Name,Color,Description,CreatedBy,CreatedAt,LastModifiedBy,LastModifiedAt")] TblDmLabel label)
         {
             if (id != label.Id)
             {
